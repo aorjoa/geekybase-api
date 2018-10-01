6be87a486183b0c95c8a40cf7c277958c9a7ccd1
pallat <yod.pallat@gmail.com>
2018-10-01 20:12:42 +0700

    refactor function example

--- main.go
+++ main.go
@@ -9,19 +9,21 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		req := c.Request()
-		format := `
-			<code>
-				Protocol: %s<br>
-				Host: %s<br>
-				Remote Address: %s<br>
-				Method: %s<br>
-				Path: %s<br>
-				Message : Test
-			</code>
-		`
-		return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
-	})
+	e.GET("/", example)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func example(c echo.Context) error {
+	req := c.Request()
+	format := `
+		<code>
+			Protocol: %s<br>
+			Host: %s<br>
+			Remote Address: %s<br>
+			Method: %s<br>
+			Path: %s<br>
+			Message : Test
+		</code>
+	`
+	return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
 }
